1c82a8e1825dd4fef0c3628d6f0654ef5e20358c
Konye <isnantointerista@gmail.com>
2018-05-11 23:48:14 +0700

    using postgreSQL

--- web_app/app.py
+++ web_app/app.py
@@ -3,15 +3,16 @@ from flask import Flask, render_template
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_pyfile('settings.py')
 
 
     @app.route('/')
     def index():
-        return render_template('index.html')
+        return render_template('index.html', TITLE='nitnot')
 
     @app.route('/about')
     def about():
-        return 'ini project awal'
+        return render_template('about.html', TITLE='nitnot')
 
     return app
 
